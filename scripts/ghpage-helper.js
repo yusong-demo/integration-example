b6acbfc433327ce5938a3f6648090bfa8c36bbe8
Yusong Hsu <sky172839465@gmail.com>
2024-01-15 16:43:32 +0800

    ci: using sha for schedule result folder (#27)
    * ci: getGHPageInfo add test url
    
    * ci: schedule gh page folder using sha
    
    * ci: using sha for schedule result folder

--- scripts/ghpage-helper.js
+++ scripts/ghpage-helper.js
@@ -1,11 +1,11 @@
 const _ = require('lodash')
 const packageConfig = require('../package.json')
 
-const getGHPageInfo = async ({ context }) => {
+const getGHPageInfo = async ({ context }, sha) => {
   const prefix = packageConfig.name
   const prNumber = _.get(context, 'payload.pull_request.number')
   const prRunId = _.get(context, 'runId')
-  const destination_dir = `pr${prNumber}-${prRunId}`
+  const destination_dir = _.isUndefined(sha) ? `pr${prNumber}-${prRunId}` : sha
   const ghPage = `https://sky172839465.github.io/${prefix}/${destination_dir}`
   return { ghPage, destination_dir }
 }
